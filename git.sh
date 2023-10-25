472f68531adb0c30ad845488471ce491c16656e2
H.Meneses <hm_kali@Matrix.com.br>
2023-10-24 23:37:19 -0300

    Commit By ./HMeneses

---
+++ git.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#--------------------------------------------------------------------#
+#     Org: Line by line
+#   Title: Gits for Githubs
+#      By: Hernaldo Meneses
+#    Date: 24/10/2023 22:35
+#Function: Facilitar utilização do git
+#--------------------------------------------------------------------#
+#
+##sudo apt update
+##sudo apt upgrade
+##sudo apt-get install git
+echo "Verificando a versão do git instalada"
+git --version
+
+echo "Status Atual git"
+git status
+#/** O Comando cd permite ao usuario linux navegar pelos diretorios
+#/** tornando assim sua utilização essenssial para administraçao
+#/** e implementação de recursos no sitema.
+#/** A capacidade de utilização de combinação de parametros com outros
+#/** comando traz eficiência e eficácia nas tarefas de utilização e
+#/** Administração de sistemas Linux. 
+#-----------------------------------/** That's All **/--------------#
+
